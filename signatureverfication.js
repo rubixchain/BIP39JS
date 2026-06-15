7af7b561b7da9f15f3b33a945a104d14e3c084c0
Maneesha Panda <maneesha.p@rubix.net>
2026-06-15 10:17:03 +0530

    print base64 signature only

--- signatureverfication.js
+++ signatureverfication.js
@@ -6,11 +6,11 @@ const { secp256k1 } = require('@noble/curves/secp256k1');
   const message = "5tZJVstcCmq3Cxum7u52y+nieWHma3Wawrbyi0tl0GQ=";
   
   const messagebyteArray = new Uint8Array(Buffer.from(message, "base64"));
-  console.log("msg (Uint8Array):", messagebyteArray);
+  // console.log("msg (Uint8Array):", messagebyteArray);
 
   const signature = await secp256k1.sign(messagebyteArray, privKeyStr);
   const signatureDER = signature.toDERRawBytes();
-  console.log("signature (toDERRawBytes)" , signatureDER);
+  // console.log("signature (toDERRawBytes)" , signatureDER);
   const signatureBase64 = Buffer.from(signatureDER).toString('base64');
   console.log("signature (Base64):", signatureBase64);
   console.log("signature size (toDERRawBytes)" , signatureDER.length);
